2d948fc57aec30fbbc9e5e5a89b1dd09812571b6
Lokesh10062000 <chlokeshvarmam003@gmail.com>
2023-06-16 17:43:29 +0530

    v4

---
+++ java04_literals/src/com/dl/one/Eg1.java
@@ -0,0 +1,26 @@
+package com.dl.one;
+
+public class Eg1 {
+	
+	public static void main(String[]args) {
+		
+		
+		//Integer literals
+		int i=10;
+		System.out.println(i);
+		//floting point literals
+		float f=12.3f;
+		System.out.println(f);
+		//string literals
+		String st="lokesh";
+		System.out.println(st);
+		//Boolean literals
+		boolean b=true;
+		System.out.println(b);
+		
+		//character literals
+		char ch='a';
+		System.out.println(ch);
+	}
+
+}
